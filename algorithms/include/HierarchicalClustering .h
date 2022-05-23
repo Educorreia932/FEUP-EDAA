83c9dc6ed5c34d54933e439462be04b44b6631a2
Educorreia932 <skelozard1@gmail.com>
2022-05-23 20:09:55 +0100

    Basic structure for Hierarchical Clustering class

--- algorithms/include/HierarchicalClustering .h
+++ algorithms/include/HierarchicalClustering .h
@@ -1,3 +1,18 @@
-class HierarchicalTree {
+#include <vector>
 
+class Item {
+private:
+    Item &left;
+    Item &right;
+public:
+    friend class HierarchicalClustering;
+
+    Item();
+};
+
+class HierarchicalClustering {
+public:
+    HierarchicalClustering(std::vector<Item> items);
+
+    void buildTree();
 };
