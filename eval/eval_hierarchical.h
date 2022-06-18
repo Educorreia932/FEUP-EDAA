4c77bbd737ad7eedbacb2e9be2518e58cb2ddaf3
Eduardo Correia <skelozard1@gmail.com>
2022-06-18 19:44:45 +0100

    Calculating hierarchical Clustering for a sample of coordinates

--- eval/eval_hierarchical.h
+++ eval/eval_hierarchical.h
@@ -1,14 +1,36 @@
 #pragma once
 
+#include "Coord.h"
+#include "HierarchicalClustering.h"
+
+#include <list>
 #include <iostream>
+#include <vector>
 
 void evalHierarchical(const MapGraph& map_graph) {
     auto distance_graph = map_graph.getDistanceGraph();
     std::list<Coord> coordinate_list;
 
-    for (auto node : distance_graph.getNodes()) 
+    for (auto node : distance_graph.getNodes())
         coordinate_list.push_back(map_graph.nodeToCoord(node));
 
-    for (auto coordinates : coordinate_list) 
-        std::cout << coordinates.lat() << std::endl;
+    std::vector<std::vector<double> > distance_matrix;
+
+    std::list<Coord> sample;
+
+    int N = 100;
+
+    // Get first 100 coordinates
+    for (auto i = coordinate_list.begin(); i != coordinate_list.end() && N > 0; ++i, --N)
+        sample.push_back(*i);
+
+    for (auto c1 : sample) {
+        distance_matrix.push_back(std::vector<double>());
+
+        for (auto c2 : sample) 
+            distance_matrix[distance_matrix.size() - 1].push_back(Coord::getDistanceArc(c1, c2));
+    }
+
+    auto upgma = UPGMA(distance_matrix);
+    auto tree = upgma.calculate();
 }
